24cbd7d0380555fad31002c33827bdbf2f33486f
vsdworkk <vsdworkk@gmail.com>
2025-03-01 23:08:57 +1100

    step 11 v2

--- types/html2pdf-types.ts
+++ types/html2pdf-types.ts
@@ -3,30 +3,27 @@
  */
 
 declare module 'html2pdf.js' {
-  function html2pdf(): html2pdf.Html2PdfWrapper;
-  
-  namespace html2pdf {
-    interface Html2PdfWrapper {
-      from(element: HTMLElement): Html2PdfWrapper;
-      set(options?: Html2PdfOptions): Html2PdfWrapper;
-      save(): Promise<void>;
-    }
-    
-    interface Html2PdfOptions {
-      margin?: number;
-      filename?: string;
-      image?: {
-        type?: string;
-        quality?: number;
-      };
-      html2canvas?: Record<string, any>;
-      jsPDF?: {
-        unit?: string;
-        format?: string;
-        orientation?: string;
-      };
-    }
+  interface Html2PdfWrapper {
+    from(element: HTMLElement): Html2PdfWrapper;
+    set(options?: Html2PdfOptions): Html2PdfWrapper;
+    save(): Promise<void>;
   }
   
+  interface Html2PdfOptions {
+    margin?: number;
+    filename?: string;
+    image?: {
+      type?: string;
+      quality?: number;
+    };
+    html2canvas?: Record<string, any>;
+    jsPDF?: {
+      unit?: string;
+      format?: string;
+      orientation?: string;
+    };
+  }
+
+  function html2pdf(): Html2PdfWrapper;
   export = html2pdf;
 } 
--- types/index.ts
+++ types/index.ts
@@ -3,4 +3,4 @@ Exports the types for the app.
 */
 
 export * from "./server-action-types"
-export * from "./html2pdf-types"
+// html2pdf-types.ts is a declaration file and doesn't need to be exported
