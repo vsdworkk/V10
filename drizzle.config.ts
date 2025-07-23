97667d1671d31ae73cbbaf3724b05e283c383b5f
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2025-07-23 21:11:02 +0530

    fix: safe usage of env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,16 @@ Configures Drizzle for the app.
 import { config } from "dotenv"
 import { defineConfig } from "drizzle-kit"
 
+const DB_URL = process.env.DATABASE_URL
+if (!DB_URL) {
+  throw new Error("Missing DATABASE_URL environment variable")
+}
+
 config({ path: ".env.local" })
 
 export default defineConfig({
   schema: "./db/schema/index.ts",
   out: "./db/migrations",
   dialect: "postgresql",
-  dbCredentials: { url: process.env.DATABASE_URL! }
+  dbCredentials: { url: DB_URL }
 })
