d3e2899cf317ec73d814aefd767691d02c8904d9
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2025-09-08 09:31:55 +0530

    fix: removed hardcoded gogole verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,10 +28,7 @@ const playfair = Playfair_Display({
 
 export const metadata: Metadata = {
   title: "APSPitchPro",
-  description: "3X Your Interview Chances With AI-Powered Pitches",
-  verification: {
-    google: "sApD36Oz-lEo2szSB7pik9OsJ0uKY0q5Gb1iyIqNLJw"
-  }
+  description: "3X Your Interview Chances With AI-Powered Pitches"
 }
 
 export default async function RootLayout({
